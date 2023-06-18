05e0a0bc004cc65008fcbd5df644b57afd157039
Xavier2p <xavier.de-place@epita.fr>
2023-06-18 17:27:55 +0200

    Change path to configs

--- config.sh
+++ config.sh
@@ -4,4 +4,5 @@ ln -fs $DOTFILES/zshrc ~/.zshrc
 ln -fs $DOTFILES/bashrc ~/.bashrc
 ln -fs $DOTFILES/vimrc ~/.vimrc
 ln -fs $DOTFILES/gitconfig ~/.gitconfig
-ln -fs $DOTFILES/neofetch.conf ~/.config/neofetch/config.conf
+ln -fs $DOTFILES/config/neofetch.conf ~/.config/neofetch/config.conf
+ln -fs $DOTFILES/config/alacritty.yml ~/.config/alacritty/alacritty.yml

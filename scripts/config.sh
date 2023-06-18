79618814f8aaf6c92adf123aef8eb7e869ca1238
Xavier2p <xavier.de-place@epita.fr>
2023-06-18 18:55:38 +0200

    update due to previous changes

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,6 +3,6 @@ DOTFILES=$HOME/.config/dotfiles
 ln -fs $DOTFILES/zshrc ~/.zshrc
 ln -fs $DOTFILES/bashrc ~/.bashrc
 ln -fs $DOTFILES/vimrc ~/.vimrc
-ln -fs $DOTFILES/gitconfig ~/.gitconfig
+ln -fs $DOTFILES/config/git.conf ~/.gitconfig
 ln -fs $DOTFILES/config/neofetch.conf ~/.config/neofetch/config.conf
 ln -fs $DOTFILES/config/alacritty.yml ~/.config/alacritty/alacritty.yml
